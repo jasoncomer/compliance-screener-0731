f92de3e9ea627acc5e0a0845622d98faf11ddc9a
Bryan Oliveira <vicdaruf@yahoo.com>
2025-09-26 11:18:30 -0500

    fix build

--- src/components/__tests__/EntityToggle.test.tsx
+++ src/components/__tests__/EntityToggle.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent,render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { vi, describe, it, beforeEach, expect } from 'vitest';
 
 import '@testing-library/jest-dom';
