157b8356c67149d23576832540f25c105b92a787
Bryan Oliveira <vicdaruf@yahoo.com>
2025-09-26 13:04:45 -0500

    bump

--- src/components/__tests__/EntityToggle.test.tsx
+++ src/components/__tests__/EntityToggle.test.tsx
@@ -1,4 +1,5 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/dom';
 import { vi, describe, it, beforeEach, expect } from 'vitest';
 
 import '@testing-library/jest-dom';
